01fc16a9dc697f1512ae812b6eef4b9d78c382ef
Yuichi <yuichi.kageyama@mondo.com.au>
2024-08-29 16:08:33 +1000

    update

--- stacks/customStack.ts
+++ stacks/customStack.ts
@@ -3,6 +3,9 @@ import {Construct} from 'constructs'
 import {MonitoringFacade} from 'cdk-monitoring-constructs'
 import {Bucket} from 'aws-cdk-lib/aws-s3'
 import {BucketDeployment, Source} from 'aws-cdk-lib/aws-s3-deployment'
+import {NodejsFunction} from 'aws-cdk-lib/aws-lambda-nodejs'
+import {Code} from 'aws-cdk-lib/aws-lambda'
+import {TreatMissingData} from 'aws-cdk-lib/aws-cloudwatch'
 
 export class CustomStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -16,7 +19,23 @@ export class CustomStack extends Stack {
       ]
     })
 
+    new NodejsFunction(this, 'function', {
+      handler: 'main',
+      code: Code.fromInline('export async const main = () => console.log("hello world")')
+    })
+
     const monitoring = new MonitoringFacade(this, 'Monitoring')
-    monitoring.monitorScope(this)
+    monitoring.monitorScope(this, {
+      lambda: {
+        props: {
+          addFaultCountAlarm: {
+            Error: {
+              maxErrorCount: 0,
+              treatMissingDataOverride: TreatMissingData.MISSING
+            }
+          }
+        }
+      }
+    })
   }
 }
